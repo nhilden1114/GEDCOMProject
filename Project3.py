c7924e388128f0100071b19d52d2c9becd5a7e68
Nicole Hilden <nicole.hilden@gmail.com>
2018-09-13 22:41:56 -0400

    worked on finishing project2 aspects of the file

--- Project3.py
+++ Project3.py
@@ -7,7 +7,8 @@ I pledge my honor that I have abided by the Stevens Honor System
 
 """
 #the line below opens the indicated file and reads it
-file = open('/Users/Mbenezra/SSW-555/proj02test (1).ged', 'r')
+#file = open('/Users/Mbenezra/SSW-555/proj02test (1).ged', 'r')
+file = open('/Users/Test/Documents/SSW555/proj02test.ged')
 
 #the dictionary below defines all of the levels and their valid tags for GEDCOM family trees
 valid = {
@@ -19,8 +20,8 @@ valid = {
 #this prints each line in the file, strips it of the white space, splits the line up into any array called "tokens"
 #and then the array is used to identify the level, tag, and any associated arugments for the line
 for line in file:
-    print ('-->', line)
     line = line.strip()
+    print ('-->', line)
     tokens = line.split()
     level = tokens[0]
     tag = tokens[1]
@@ -29,23 +30,22 @@ for line in file:
     #this if statement is to decide if the line is valid or invalid
     #this decision is made by seeing if the tag in the line is at the appropriate level
     #if level in valid:
-    if tag in valid[level]:
-        ok = 'y'
+    if level in valid and tag in valid[level]:
+        ok = 'Y'
     else:
         if len(tokens)>= 3 and tokens[2] in ['INDI', 'FAM']:
-            ok = 'y'
+            ok = 'Y'
         else:
-            ok = 'n'
-    #else:
-       # level, tag, valid, args
+            ok = 'N'
 
     if len(tokens) >= 3:
         if tokens[2] in ['INDI', 'FAM']:
             print("<-- {}|{}|{}|{}".format(level, args, ok, tag))
-        elif valid:
-            print("<-- {}|{}|{}|{}".format(level, tag, ok, args))
         else:
             print("<-- {}|{}|{}|{}".format(level, tag, ok, args))
 
-    #to print the output
-    #print ("<--", "|", level, "|", ok, "|",  tag, "|", args)
+    #to print the output if len<3
+    else:
+        if args == "":
+            args = 'N/A'
+        print ("<-- " + level.strip()+ "|"+ tag+"|"+ok+"|"+args)
