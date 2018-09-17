3ab8b24cc242c18c98ba5d7c71c0644366a658c6
Nicole Hilden <nicole.hilden@gmail.com>
2018-09-17 18:30:30 -0400

    vars -> dict

--- IndiTable.py
+++ IndiTable.py
@@ -8,12 +8,16 @@ def determineAge(dob, currDate):
     
     birth = datetime.datetime.strptime(dob, "%d %b %Y").date()
     age = currDate.year - birth.year - ((birth.month, birth.day) > (currDate.month, currDate.day))
+    if age <0:
+        return age*(-1)
     return age
 
 def createIndiTable(file):
 
     tab = PrettyTable()
     tab.field_names = ["ID","Name","Gender","Birthday","Age","Alive","Death","Child","Spouse"]
+
+    vals = dict({"ID":"N/A","Name":"N/A","Gender":"N/A","Birthday":"N/A","Age":"N/A","Alive":"N/A","Death":"N/A","Child":"N/A","Spouse":"N/A"})
 
     for line in file:
         ok = validLine(line)
@@ -26,51 +30,63 @@ def createIndiTable(file):
         #needs to be initialized somewhere else
         #ID,Name,Gender,Birthday,Age,Alive,Death,Child,Spouse = ("","","","","","","","","")
 
-        if ok == "Y" and (tag in ["NAME","SEX","BIRTH","DEAT","FAMC","FAMS"] or (len(tokens)>=3 and tokens[2]=="INDI")):
+        if ok == "Y" and (tag in ["NAME","SEX","FAMC","FAMS"] or (len(tokens)>=3 and tokens[2]=="INDI")):
                 
             if tag == "NAME":
-                Name = args
+                vals["Name"] = args
                 
             elif tag == "SEX":
-                Gender = args
-                
-            elif tag == "BIRT":
+                vals["Gender"] = args
+
+ #           elif tag == "BIRT":
                 
                 #need to access next line for the DATE
-                newLine = next(file)
-                newTok = newLine.split()
-                newTag = newTok[1]
-                newArgs = " ".join(tokens[2:])
-                if newTag == "DATE":
-                    Birthday = newArgs
-                    Age = determineAge(newArgs, datetime.date.today())
+ #               newLine = next(file)
+  #              newTok = newLine.split()
+   #             newTag = newTok[1]
+    #            newArgs = " ".join(tokens[2:])
+     #           if newTag == "DATE":
+      #              vals["Birthday"] = newArgs
+       #             vals["Age"] = determineAge(newArgs, datetime.date.today())
 
-            elif tag == "DEAT":
-                Alive = "False"
+        #    elif tag == "DEAT":
+         #       vals["Alive"] = "False"
 
                 #need to access next line for the DATE
-                newLine = next(file)
-                newTok = newLine.split()
-                newTag = newTok[1]
-                newArgs = " ".join(tokens[2:])
-                if newTag == "DATE":
-                    Death = newArgs
-                    Age = determineAge(Birthday, newArgs)
+          #      newLine = next(file)
+           #     newTok = newLine.split()
+            #    newTag = newTok[1]
+             #   newArgs = " ".join(tokens[2:])
+              #  if newTag == "DATE":
+                #     vals["Death"] = newArgs
+                 #   vals["Age"] = determineAge(vals["Birthday"], newArgs)
 
             elif tag == "FAMS":
-                Spouse = args
+                vals["Spouse"] = args
+                vals["Child"] = "N/A"
+                tab.add_row([vals["ID"],vals["Name"],vals["Gender"],vals["Birthday"],vals["Age"],vals["Alive"],vals["Death"],vals["Child"],vals["Spouse"]])
+
                 
             elif tag == "FAMC":
-                Child = args
+                vals["Child"] = args
+                vals["Spouse"] = "N/A"
+                tab.add_row([vals["ID"],vals["Name"],vals["Gender"],vals["Birthday"],vals["Age"],vals["Alive"],vals["Death"],vals["Child"],vals["Spouse"]])
+
 
             elif tokens[2] == "INDI":
-                ID = tokens[1]
+                vals["ID"] = tokens[1]
+
+        elif ok == "Y" and tag == "DATE":
+            vals["Age"] = determineAge(args, datetime.date.today())
+            print(args, " and ", vals["Age"] )
 
 
-        try: (ID,Name,Gender,Birthday,Age,Alive,Death,Child,Spouse)
-        except: continue
 
-        tab.add_row([ID,Name,Gender,Birthday,Age,Alive,Death,Child,Spouse])
+
+        #try: (ID,Name,Gender,Birthday,Age,Alive,Death,Child,Spouse)
+        #except: continue
+
+        #tab.add_row([ID,Name,Gender,Birthday,Age,Alive,Death,Child,Spouse])
             
     return tab
 
