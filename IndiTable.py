7bed68d5c591dcdfed9a80a1095fb9fa81b1c48c
Nicole Hilden <nicole.hilden@gmail.com>
2018-09-17 21:29:07 -0400

    IT WORKS

--- IndiTable.py
+++ IndiTable.py
@@ -4,8 +4,9 @@ from prettytable import PrettyTable
 import datetime
 
 
-def determineAge(dob, currDate):
+def determineAge(dob, date):
     
+    currDate = datetime.datetime.strptime(date, "%d %b %Y").date()
     birth = datetime.datetime.strptime(dob, "%d %b %Y").date()
     age = currDate.year - birth.year - ((birth.month, birth.day) > (currDate.month, currDate.day))
     if age <0:
@@ -17,13 +18,13 @@ def createIndiTable(file):
     tab = PrettyTable()
     tab.field_names = ["ID","Name","Gender","Birthday","Age","Alive","Death","Child","Spouse"]
 
-    vals = dict({"ID":"N/A","Name":"N/A","Gender":"N/A","Birthday":"N/A","Age":"N/A","Alive":"N/A","Death":"N/A","Child":"N/A","Spouse":"N/A"})
+    vals = dict({"ID":"N/A","Name":"N/A","Gender":"N/A","Birthday":"N/A","Age":"N/A","Alive":"True","Death":"N/A","Child":"N/A","Spouse":"N/A"})
 
     arr = []
     for line in file:
         arr.append(line.strip())
 
-    print(arr)
+    #print(arr)
 
     #for line in file:
         #ok = validLine(line)
@@ -43,7 +44,7 @@ def createIndiTable(file):
         #needs to be initialized somewhere else
         #ID,Name,Gender,Birthday,Age,Alive,Death,Child,Spouse = ("","","","","","","","","")
 
-        if ok == "Y" and (tag in ["NAME","SEX","BIRTH","DEAT","FAMC","FAMS"] or (len(tokens)>=3 and tokens[2]=="INDI")):
+        if ok == "Y" and (tag in ["NAME","SEX","BIRT","DEAT","FAMC","FAMS"] or (len(tokens)>=3 and tokens[2]=="INDI")):
                 
             if tag == "NAME":
                 vals["Name"] = args
@@ -54,45 +55,54 @@ def createIndiTable(file):
             elif tag == "BIRT":
                 
                 #need to access next line for the DATE
-                newLine = next(file)
+                newLine = arr[i+1]
+                print("newLineB: ", newLine)
+                newOK = validLine(newLine)
                 newTok = newLine.split()
-   #             newTag = newTok[1]
-    #            newArgs = " ".join(tokens[2:])
-     #           if newTag == "DATE":
-      #              vals["Birthday"] = newArgs
-       #             vals["Age"] = determineAge(newArgs, datetime.date.today())
+                newTag = newTok[1]
+                newArgs = " ".join(newTok[2:])
+                
+                if newOK == "Y" and newTag == "DATE":
+                    print("newArgsB: ", newArgs)
+                    vals["Birthday"] = newArgs
+                    vals["Age"] = determineAge(newArgs, "17 NOV 2018")
 
-        #    elif tag == "DEAT":
-         #       vals["Alive"] = "False"
+            elif tag == "DEAT":
+                vals["Alive"] = "False"
 
                 #need to access next line for the DATE
-          #      newLine = next(file)
-           #     newTok = newLine.split()
-            #    newTag = newTok[1]
-             #   newArgs = " ".join(tokens[2:])
-              #  if newTag == "DATE":
-                #     vals["Death"] = newArgs
-                 #   vals["Age"] = determineAge(vals["Birthday"], newArgs)
+                newLine = arr[i+1]
+                print("newLineD: ", newLine)
+                newOK = validLine(newLine)
+                newTok = newLine.split()
+                newTag = newTok[1]
+                newArgs = " ".join(newTok[2:])
+                
+                if newOK == "Y" and newTag == "DATE":
+                    print("newArgsD: ", newArgs)
+                    vals["Death"] = newArgs
+                    vals["Age"] = determineAge("4 NOV 1990", newArgs)
 
             elif tag == "FAMS":
                 vals["Spouse"] = args
                 vals["Child"] = "N/A"
                 tab.add_row([vals["ID"],vals["Name"],vals["Gender"],vals["Birthday"],vals["Age"],vals["Alive"],vals["Death"],vals["Child"],vals["Spouse"]])
-
+                vals["Alive"] = "True"
+                vals["Death"] = "N/A"
                 
             elif tag == "FAMC":
                 vals["Child"] = args
                 vals["Spouse"] = "N/A"
                 tab.add_row([vals["ID"],vals["Name"],vals["Gender"],vals["Birthday"],vals["Age"],vals["Alive"],vals["Death"],vals["Child"],vals["Spouse"]])
-
+                vals["Alive"] = "True"
+                vals["Death"] = "N/A"
 
             elif tokens[2] == "INDI":
                 vals["ID"] = tokens[1]
 
-        elif ok == "Y" and tag == "DATE":
-            vals["Age"] = determineAge(args, datetime.date.today())
-            print(args, " and ", vals["Age"] )
-
+        #elif ok == "Y" and tag == "DATE":
+        #    vals["Age"] = determineAge(args, datetime.date.today())
+        #    print(args, " and ", vals["Age"] )
 
 
 
